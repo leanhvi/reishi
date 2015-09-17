048569ecd4a771b965539ed2776fa89414df2194
manhcompany <manhcompanydoi@gmail.com>
2015-09-17 22:20:13 +0700

    main

--- reishi_idf/src/main/java/reishi/idf/MapReduceIdf.java
+++ reishi_idf/src/main/java/reishi/idf/MapReduceIdf.java
@@ -5,10 +5,67 @@
  */
 package reishi.idf;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  *
  * @author manhc
  */
 public class MapReduceIdf {
+    public static class IdfMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        
+        private Text word = new Text();
+        
+        public void map(Object key, Text value, Reducer.Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {                
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
     
+    public static class IdfReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+        
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for(IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        
+        Job job = new Job(conf, "Calculator Idf");
+        
+        job.setJarByClass(MapReduceIdf.class);
+        job.setMapperClass(IdfMapper.class);
+        job.setCombinerClass(IdfReducer.class);
+        job.setReducerClass(IdfReducer.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
